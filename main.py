317a13b8418c389d7d019fc4b250a3785c8097f7
Sibin T S <62069255+Sibints2000@users.noreply.github.com>
2021-07-31 12:07:08 +0000

    Fifth Commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bmi = round(weight / height ** 2)
 if bmi < 18.5:
   print(f"Your bmi is {bmi}, you are underweight.")
 elif bmi < 25:
-  print(f"Your bmi is {bmi}, You are underweight.")
+  print(f"Your bmi is {bmi}, You have a normal weight.")
 
 
 
