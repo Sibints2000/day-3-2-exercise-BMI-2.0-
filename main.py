f30ea2a55ff4e5b1320a01596d0a638876ec5dca
Sibin T S <62069255+Sibints2000@users.noreply.github.com>
2021-07-31 12:08:59 +0000

    Sixth Commit

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ if bmi < 18.5:
   print(f"Your bmi is {bmi}, you are underweight.")
 elif bmi < 25:
   print(f"Your bmi is {bmi}, You have a normal weight.")
-
+elif bmi < 30:
+  print(f"your bmi is {bmi}, you are overweight.")
+elif bmi < 35:
+  print(f"Your bmi is {bmi}, you are obese.")
+  
 
 
 
